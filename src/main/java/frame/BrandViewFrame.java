0b6d7a388b7f0f7d72e68da53be6ed6a8a45cf85
Ahmadnafis79 <ahmadnafis79@gmail.com>
2022-06-25 22:27:03 +0800

    Membuat BrandViewFrame

--- src/main/java/frame/BrandViewFrame.java
+++ src/main/java/frame/BrandViewFrame.java
@@ -1,13 +1,20 @@
 package frame;
 
-import javax.swing.*;
+import helpers.Koneksi;
 
-public class BrandViewFrame extends JFrame{
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
+
+public class BrandViewFrame extends JFrame {
     private JPanel mainPanel;
     private JPanel cariPanel;
     private JScrollPane viewScrollPane;
     private JPanel buttonPanel;
-    private JTextField textField1;
+    private JTextField cariTextField;
     private JButton cariButton;
     private JTable viewTable;
     private JButton tambahButton;
@@ -17,11 +24,75 @@ public class BrandViewFrame extends JFrame{
     private JButton cetakButton;
     private JButton tutupButton;
 
-    public BrandViewFrame(){
+    public BrandViewFrame() {
+        tutupButton.addActionListener(e -> {
+            dispose();
+        });
+
+        batalButton.addActionListener(e -> {
+            isiTable();
+        });
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e) {
+                isiTable();
+            }
+        });
+
+
+        cariButton.addActionListener(e -> {
+            Connection c = Koneksi.getConnection();
+            String keyword = "%" + cariTextField.getText() + "%";
+            String seachSQL = "SELECT * FROM brand WHERE nama like ?";
+            try {
+                PreparedStatement ps = c.prepareStatement(seachSQL);
+                ps.setString(1, keyword);
+                ResultSet rs = ps.executeQuery();
+                DefaultTableModel dtm = (DefaultTableModel) viewTable.getModel();
+                dtm.setRowCount(0);
+                Object[] row = new Object[2];
+                while (rs.next()) {
+                    row[0] = rs.getInt("id_brand");
+                    row[1] = rs.getString("nama");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        hapusButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0) {
+                JOptionPane.showMessageDialog(null, "Pilih data dulu");
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(null,
+                    "Yakin mau hapus?",
+                    "Konfirmasi Hapus",
+                    JOptionPane.YES_NO_OPTION
+            );
+            if (pilihan == 0) {
+                TableModel tm = viewTable.getModel();
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih, 0).toString());
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM brand WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1, id);
+                    ps.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        isiTable();
         init();
     }
 
-    public void init(){
+    public void init() {
         setContentPane(mainPanel);
         setTitle("Data Brand");
         pack();
@@ -29,4 +100,24 @@ public class BrandViewFrame extends JFrame{
         setLocationRelativeTo(null);
     }
 
+    public void isiTable() {
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT * FROM brand";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            String header[] = {"id_Brand", "Nama Brand"};
+            DefaultTableModel dtm = new DefaultTableModel(header, 0);
+            viewTable.setModel(dtm);
+            Object[] row = new Object[2];
+            while (rs.next()) {
+                row[0] = rs.getInt("id_brand");
+                row[1] = rs.getString("nama");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
